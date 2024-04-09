70d79e01bad17109288d244f285286a47e4dc442
Despair-17 <IIINo-48III@yandex.ru>
2024-04-10 02:19:13 +0300

    Добавлен шаблон base.html и функция представления index

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+menu = [
+    {'title': 'О сайте', 'slug': '#'},
+    {'title': 'FAQ', 'slug': '#'},
+    {'title': 'Начни свой блог', 'slug': '#'},
+]
+
+
+def index(request):
+    context = {
+        'title': 'YourBlog',
+        'menu': menu,
+    }
+    return render(request, 'posts/index.html', context)
